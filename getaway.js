9f6051f63697ef0c7ef8f1564b16130722089ee9
Sonal Patel <sonalpatel.sunny@gmail.com>
2019-08-09 20:49:58 -0500

    dblclick escape on all pages

---
+++ getaway.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+
+    $(window).dblclick(function () {
+        window.open("http://pinterest.com", "_newtab");
+        window.location.replace('http://google.com');
+    });
+});
