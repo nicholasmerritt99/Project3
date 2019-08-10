90f6256571d61db8a329e42100f97aa4ed3eeaa1
Nicholas Merritt <nicholasmerritt1994@gmail.com>
2019-08-10 13:09:17 -0500

    Updated routes to use the API for shelters

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,8 @@
 var path = require("path");
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
 
 module.exports = function(app) {
 	// if user enters survey in URL or presses survey button, serves the survey HTML file
@@ -11,8 +15,24 @@ module.exports = function(app) {
 		res.sendFile(path.join(__dirname, "/../public/dummy.html"));
 	});
 
-	app.get('/s', function(req, res, next) {
-		hisfunction()
+	app.get('/abuseshelter', function(req, res, next) {
+		const googleBaseUrl = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
+		const googleSearches = ['abuse+shelters'];
+		const city = 'dallas';
+		const state = 'texas';
+		const searchTerm = googleSearches[0]
+		const scrapeUrl = `${googleBaseUrl}?query=${searchTerm}+${city}+${state}&key=AIzaSyA7UQiFxk93uZYMBFDUkQ2WDC0atq88sBA`;
+
+		axios.get(scrapeUrl).then(response => {
+			const $ = cheerio.load(response.data);
+			let titleList = [];
+			const headings = $('.BNeawe.deIvCb.AP7Wnd');
+		   console.log('DATA', response.data);
+			console.log('HEADINGS: ', headings);
+		   
+		   console.log(titleList);
+   
+		   });
 
 		
 	  });
